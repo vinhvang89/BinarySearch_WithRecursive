b300ffab9617460ce1765eee7c983fe979ab3ce5
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-04-20 10:46:15 +0700

    fix

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,21 +1,25 @@
+import java.security.cert.TrustAnchor;
+
 public  class  BinarySearch {
       static  int[] numbers = {1,2,3,5,8,13,21,34,55,89,134,223,357,590,947};
-      static int i = 0;
-      public static void search( int number) {
-          int temp = numbers[i];
-          if (temp == number) {
-              System.out.println("True");
-              i = 0;
-          }
-          else {
-              i++;
-              if (i < numbers.length)
-                  search(number);
-              else {
-                  System.out.println("False");
-                  i = 0;
+      public static void search(int number){
+          search(number,0,numbers.length-1);
+      }
+      public static void search( int number,int lowest, int highest) {
+          int mid =  (lowest + highest)/2;
+          if( highest >= lowest) {
+              if (number > numbers[mid]) {
+                  lowest = mid + 1;
+                  search(number,lowest,highest);
+              } else if (number < numbers[mid]) {
+                  highest = mid - 1;
+                  search(number,lowest,highest);
+              } else {
+                  System.out.println("True");
               }
           }
+          else
+              System.out.println("False");
 
       }
 
